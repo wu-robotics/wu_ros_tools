6d70b5c532c8bf0137aabeed1cb423a7e3811097
David V. Lu!! <davidvlu@gmail.com>
2014-08-18 12:57:30 -0500

    Rebase from 'upstream'

--- kalman_filter/setup.py
+++ kalman_filter/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 package_info = generate_distutils_setup(
-    packages=['easy_markers'],
+    packages=['kalman_filter'],
     package_dir={'': 'src'},
 )
 
